66364497a50540323a03b5d464c0cd58fcdab99c
shanks25 <kunal.rajput2511@gmail.com>
2019-02-24 23:34:40 +0530

    firstcommit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultstringlength(191);
+        \Braintree_Configuration::environment(config('services.braintree.environment'));
+        \Braintree_Configuration::merchantId(config('services.braintree.merchant_id'));
+        \Braintree_Configuration::publicKey(config('services.braintree.public_key'));
+        \Braintree_Configuration::privateKey(config('services.braintree.private_key'));
+    }
+}
